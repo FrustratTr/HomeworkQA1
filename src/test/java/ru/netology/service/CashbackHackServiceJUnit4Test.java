d9684ad783796e49075c63861d196ed5684d990f
Mark <mark.bogachev.00@mail.ru>
2023-11-24 16:44:24 +0300

    tests added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceJUnit4Test.java
@@ -0,0 +1,22 @@
+// src/test/java/ru/netology/service/CashbackHackServiceJUnit4Test.java
+package ru.netology.service;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceJUnit4Test {
+
+    @Test
+    public void shouldReturnZeroIfAmountIsMultipleOfBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int result = service.remain(1000);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void shouldReturnRemainderIfAmountIsNotMultipleOfBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int result = service.remain(900);
+        assertEquals(100, result);
+    }
+}
